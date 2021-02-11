2a8299198d59e0e76582cce76640772b3e724e48
vander46 <49349382+vander00@users.noreply.github.com>
2021-02-11 19:43:46 +0300

    Update finder.py

--- finder.py
+++ finder.py
@@ -5,41 +5,40 @@ from fuzzywuzzy import fuzz
 
 class Finder:
     # --- Data ---
-    FILES_NAMES = [["txt", 'log', 'html', 'css', 'cpp', 'h', 'js', 'py', 'c'], ['doc', 'docx', 'rtf', 'odt']]
+    FILES_NAMES = [["txt", 'log', 'html', 'css', 'cpp', 'h', 'js', 'py', 'c'], ['doc', 'docx', 'rtf', 'odt']]  # Supported file formats
 
     def __init__(self):
-            self.feeling = True
-    
+            self.feeling = True  # Check if text is found
+
     def checker_default(self, catalog_name, find_str, name_file):
         # ---Data---
-        numbers_str = list()
-        number_str = 1
-        numbers_repeat = 0
-        self.feeling = True
+        numbers_str = list()  # Lines with text
+        number_str = 1  # The line where the text was found
+        numbers_repeat = 0  # Repeating text in a file
 
-        with open(catalog_name + "/" + name_file, 'r', encoding='utf-8') as f:
+        with open(catalog_name + "/" + name_file, 'r', encoding='utf-8') as f:  # Opening a file
             for line in f:
-                if fuzz.partial_ratio(find_str, line) > 88:
+                if fuzz.partial_ratio(find_str, line) > 88:  # If the lines match more than 89, then the information is displayed
                     self.feeling = False
                     numbers_repeat += 1
                     numbers_str.append(number_str)
                     number_str = 0
                 number_str += 1
-            if self.feeling == False:
-                print(f"\nПуть к файлу: {catalog_name + '/' + name_file}.")
-                print(f'Строки содержания: {str(numbers_str).replace(",", " |")}')
-                print(f"Повторений в файле: {numbers_repeat}")
+            if numbers_repeat != 0:
+                print(f"\nPath to the file: {catalog_name + '/' + name_file}.")
+                print(f'Content lines: {str(numbers_str).replace(",", " |")}')
+                print(f"String repetitions: {numbers_repeat}")
 
-    def checker_other(self, catalog_name, find_str, name_file):
+    def checker_other(self, catalog_name, find_str, name_file):  # Checking files from the FILES_NAMES list
         if find_str in textract.process(catalog_name + "/" + name_file).decode('utf-8').lower():
             self.feeling = False
-            print(f"Путь к файлу: {catalog_name + '/' + name_file}")
+            print(f"Path to the file: {catalog_name + '/' + name_file}")
 
-    def run(self):
+    def run(self):  # Main function
         while True:
-            catalog_name = input("Введите путь к каталогу: ")
-            find_str = input("\nЧто бы Вы хотели найти?: ").lower()
-            
+            catalog_name = input("Directory path (C:/program): ")
+            find_str = input("\nText to find: ").lower()
+
             try:
                 catalog = os.listdir(catalog_name)
                 for name in catalog:
@@ -48,9 +47,10 @@ class Finder:
                     elif name.split('.')[-1] in Finder.FILES_NAMES[1]:
                         self.checker_other(catalog_name, find_str, name)
                 if self.feeling == True:
-                    print("Объект не был найден. ;(")
+                    print("The text was not found. ;(")
             except FileNotFoundError:
-                print('\nОшибка! Вы ввели неверную деректорию.')
+                print('\nError! You entered an invalid file path.')
+            self.feeling = True
 
 
 if __name__ == "__main__":
